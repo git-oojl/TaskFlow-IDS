83d9176f430af9c06d0ab4f814f7d36e6d5cfb39
Mya <mya.florescastanon@cesunbc.edu.mx>
2026-07-29 23:17:23 -0700

    Modelo de tareas, servicio central y persistencia local

---
+++ taskflow-avance-02/src/app/tab3/tab3.page.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss'],
+  imports: [
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardTitle,
+    IonContent,
+    IonHeader,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonTitle,
+    IonToolbar,
+  ],
+})
+export class Tab3Page {
+  constructor() {}
+}
